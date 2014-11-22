5bcaf4b295f343b1449e321b137a0acb25e963c6
girino <girinovey@girino.org>
2014-11-22 04:57:28 -0200

    Revert "new driver. might still require removal of "volatile". a voir"
    This reverts commit 6337f03f9a1626c8381120a702d7d60769617b95.

--- ocl.c
+++ ocl.c
@@ -215,7 +215,7 @@ void patch_opcodes(char *w, unsigned remaining)
 const char* spread_kernel_names[] = {
 		"signature",
 		"spreadBlake",
-		"bmwNewDriver",
+		"bmw",
 		"groestl",
 		"skein",
 		"jh",
